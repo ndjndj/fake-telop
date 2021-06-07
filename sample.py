2ba6bed3ac3d3d41d1c5da4c40de5f9f50763a67
ndjndj <teq.ndj@gmail.com>
2021-06-07 22:28:57 +0900

    main

--- sample.py
+++ sample.py
@@ -3,5 +3,14 @@ from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 import os
 
+dir = 'movie'
+path = 'Woman - 63241.mp4'
+step = 1
 
+message = [
+    ['NDJ news', 1, 4],
+    ['Hello, World.', 1.5, 11],
+    ['this message was created by ndj.', 12, 17]
+]
 
+m_slice(path, dir, step, message)
