290eab5cb223cad13e89d37ad6cb5a6dca33e9cd
ndjndj <teq.ndj@gmail.com>
2021-06-16 05:56:02 +0900

    dicision

--- src/telop.py
+++ src/telop.py
@@ -4,6 +4,11 @@ from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 import os
 
+def dicision_path(dir, path):
+    in_path = os.path.join(*[dir, path])
+    out_path = os.path.join(*[dir, 'out_' + path])
+    return (in_path, out_path)
+
 def import_movie(path):
     movie = cv2.VideoCapture(path)
     return movie
@@ -16,4 +21,3 @@ def calc_movie_config(movie):
     d['height'] = int(movie.get(cv2.CAP_PROP_FRAME_HEIGHT))
     d['fourcc'] = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     return d
-
